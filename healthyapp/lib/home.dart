cbede91a8046562ab0c56b54c467189f5ebc0155
flyingmat <paramattimatteo@gmail.com>
2021-02-14 00:33:23 +0000

    Change stuff yes

--- healthyapp/lib/home.dart
+++ healthyapp/lib/home.dart
@@ -41,8 +41,8 @@ class _HomePageState extends State<HomePage> {
     
     List<Widget> _pages = [
       ProgressPage(),
-      SocialPage(username: widget.username,),
       LeaderboardPage(),
+      SocialPage(username: widget.username,),
     ];
 
     return Scaffold(
